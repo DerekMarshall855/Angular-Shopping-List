13db5ac259502ce07e6083125310302d07144577
DerekMarshall855 <derekmarshall855@gmail.com>
2022-03-13 15:33:30 -0400

    Add more basic features and button layouts

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe("Apple Pie", "Tasty Treat mmm", "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Small_apple_pie_8.jpg/300px-Small_apple_pie_8.jpg"),
+    new Recipe("Apple Pie", "Tasty Treat mmm", "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Small_apple_pie_8.jpg/300px-Small_apple_pie_8.jpg")
+  ];
   constructor() { }
 
   ngOnInit(): void {
